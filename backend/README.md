c6bcb1e725d05c27ba351d41da9f27a9fed8910a
Pawan Kumar <pawankumar49871@gmail.com>
2025-02-16 20:04:29 +0530

    ADD: Springboot backend Init

--- backend/README.md
+++
@@ -1,1 +0,0 @@
-#Backend For Email Automation
---
+++ backend/emailTracking/src/main/java/com/gchackathon/emailTracking/EmailTrackingApplication.java
@@ -0,0 +1,13 @@
+package com.gchackathon.emailTracking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmailTrackingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmailTrackingApplication.class, args);
+	}
+
+}
